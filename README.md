0ea169603277583f8fda4f67a7af04b5f5635dc9
Ilya Bilyk <illyusha7bilyk@gmail.com>
2018-08-22 17:57:34 +0000

    Release commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# blya-shell
-BiLyk ilYA Shell written in Go
+# __*BLYA Shell*__ - **B**i**L**yk il**YA** **S**hell
+**BLYA Shell** is improved command lie shell written in **Go** by __*Ilya Bilyk*__.
+## Sources
+Originally __*Ilya Bilyk's*__ was written on **C** with name **ysh**. Then it was renamed to [**iBilyksh**](https://github.com/dwarq7/iBilyksh) because of existing **YAML SHell**.
+### Inspiration
+**BLYA Shell** was made from **gosh** written by __*Simon Jürgensmeyer*__.
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/bclicn/color"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	os.Setenv("USER", "ilya")
+	os.Setenv("HOME", "/home/ilya")
+	fmt.Println(color.White("Welcome to"), color.BLightRed("BLYA Shell"), color.White("- Bilyk Ilya Shell!"))
+	for {
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			log.Fatal(err)
+		}
+		currentDir := strings.Split(dir, "/")
+		dir = currentDir[len(currentDir)-1]
+		switch dir {
+		case os.Getenv("USER"):
+			dir = "~"
+		case "":
+			dir = "/"
+		}
+		fmt.Print(color.BLightBlue("λ "), color.BLightBlue(dir), " ")
+
+		// Read the keyboad input.
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		// Remove the newline character.
+		input = strings.TrimSuffix(input, "\n")
+
+		// Skip an empty input.
+		if input == "" {
+			continue
+		}
+
+		// Handle the execution of the input.
+		err = execInput(input)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+// ErrNoPath is returned when 'cd' was called without a second argument.
+var ErrNoPath = errors.New("path required")
+
+func execInput(input string) error {
+	// Split the input separate the command and the arguments.
+	args := strings.Split(input, " ")
+
+	// Check for built-in commands.
+	switch args[0] {
+	case "":
+
+	case "cd":
+		// 'cd' to home with empty path not yet supported.
+		if len(args) < 2 {
+			return ErrNoPath
+		}
+		err := os.Chdir(args[1])
+		if err != nil {
+			return err
+		}
+		// Stop further processing.
+		return nil
+	case "exit":
+		os.Exit(0)
+	case "bye":
+		os.Exit(0)
+	}
+
+	// Prepare the command to execute.
+	cmd := exec.Command(args[0], args[1:]...)
+
+	// Set the correct output device.
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// Execute the command and save it's output.
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
